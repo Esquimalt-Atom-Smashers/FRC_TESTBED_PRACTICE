0e7334915d9934a2124f0f244145be43b2053b12
21stcenturymacgyver <21stcenturymacgyver@gmail.com>
2025-12-17 17:05:32 -0800

    cleaned up code nothign significant

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -4,14 +4,13 @@
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.TalonSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 
 public final class Autos {
   /** Example static factory for an autonomous command. */
-  public static Command exampleAuto(TalonSubsystem subsystem) {
+  public static Command exampleAuto(Subsystem subsystem) {
     return new InstantCommand();
   }
 
