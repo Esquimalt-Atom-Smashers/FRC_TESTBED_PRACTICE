724897f21ac4f58e181d7f06ebff917680c7aa04
21stcenturymacgyver <21stcenturymacgyver@gmail.com>
2025-12-17 16:29:28 -0800

    Sample code for a talkon motor with a through bore encoder in Absolute mode

---
+++ src/main/java/frc/robot/subsystems/TalonSubsystem.java
@@ -0,0 +1,57 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.Timer;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+public class TalonSubsystem extends SubsystemBase {
+  /** Creates a new ExampleSubsystem. */
+  private final TalonSRX m_motor = new TalonSRX(1);
+  private Timer printTimer = new Timer();
+    
+    // Initialize REV Through Bore Encoder on DIO Port 0
+    // Using DutyCycleEncoder for Absolute Mode
+  private final DutyCycleEncoder m_absEncoder = new DutyCycleEncoder(0);
+
+  public TalonSubsystem() {
+    // Factory default the Talon to start from a known state
+    m_motor.configFactoryDefault();
+        
+    // Optional: Invert motor if it spins the wrong way
+    m_motor.setInverted(false);
+    printTimer.start();
+  }
+
+  /**
+   * sets the power of the motor from -1 to 1
+   */
+  public Command setPowerCommand(double power) {
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(() -> {m_motor.set(ControlMode.PercentOutput, power);});
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    
+    // Logic to print every 1 second
+    if (printTimer.hasElapsed(1.0)) {
+      // getAbsolutePosition() returns a value from 0.0 to 1.0
+      double position = m_absEncoder.get();
+      System.out.println("Encoder Absolute Position: " + position);
+      printTimer.reset();
+    }
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
